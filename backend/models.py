0db7c648102b1b67ee099b2bc3d61f7cca731f25
Fatma Alzahraa Alemam Alagery <44460069+felzahra@users.noreply.github.com>
2020-09-15 09:50:25 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia_test"
+database_name = "trivia"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
@@ -12,8 +12,6 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-
-
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -21,66 +19,60 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
-
 '''
 Question
 
 '''
+class Question(db.Model):  
+  __tablename__ = 'questions'
 
+  id = Column(Integer, primary_key=True)
+  question = Column(String)
+  answer = Column(String)
+  category = Column(String)
+  difficulty = Column(Integer)
 
-class Question(db.Model):
-    __tablename__ = 'questions'
+  def __init__(self, question, answer, category, difficulty):
+    self.question = question
+    self.answer = answer
+    self.category = category
+    self.difficulty = difficulty
 
-    id = Column(Integer, primary_key=True)
-    question = Column(String)
-    answer = Column(String)
-    category = Column(String)
-    difficulty = Column(Integer)
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
 
-    def __init__(self, question, answer, category, difficulty):
-        self.question = question
-        self.answer = answer
-        self.category = category
-        self.difficulty = difficulty
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def format(self):
-        return {
-            'id': self.id,
-            'question': self.question,
-            'answer': self.answer,
-            'category': self.category,
-            'difficulty': self.difficulty
-        }
-
+  def format(self):
+    return {
+      'id': self.id,
+      'question': self.question,
+      'answer': self.answer,
+      'category': self.category,
+      'difficulty': self.difficulty
+    }
 
 '''
 Category
 
 '''
+class Category(db.Model):  
+  __tablename__ = 'categories'
 
+  id = Column(Integer, primary_key=True)
+  type = Column(String)
 
-class Category(db.Model):
-    __tablename__ = 'categories'
+  def __init__(self, type):
+    self.type = type
 
-    id = Column(Integer, primary_key=True)
-    type = Column(String)
-
-    def __init__(self, type):
-        self.type = type
-
-    def format(self):
-        return {
-            'id': self.id,
-            'type': self.type
-        }
+  def format(self):
+    return {
+      'id': self.id,
+      'type': self.type
+    }
